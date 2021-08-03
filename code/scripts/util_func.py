4ce0406b12cc647b007012c22e5a632156be7be8
Yixue Zhao <felicitia2010@gmail.com>
2021-08-02 22:01:35 -0400

    Update util_func.py

--- code/scripts/util_func.py
+++ code/scripts/util_func.py
@@ -7,9 +7,23 @@ def delete_subdire(dirname):
     for dir in dir_list:
         shutil.rmtree(dir)
 
+def rename_file(dir):
+    for src in glob.glob(dir + '/*'):
+        dst = src + '_2'
+        os.rename(src, dst)
+
+def check_widget_exist(usage_root_dir):
+    for screen in glob.glob(usage_root_dir + '/*/ir_data_auto/*-screen.jpg'):
+        widget = screen.replace('screen', 'widget')
+        if not os.path.isfile(widget):
+            print('not exist', widget)
+
 if __name__ == '__main__':
-    usage_root_dir = os.path.abspath('/Users/yixue/Documents/Research/UsageTesting/v2s_data/Combined/SignIn')
-    delete_subdire('clicked_frames')
-    delete_subdire('clicked_frames_crop')
+    usage_root_dir = os.path.abspath('/Users/yixue/Documents/Research/UsageTesting/v2s_data/Combined/SignUp')
+    # check_widget_exist(usage_root_dir)
+    # rename_file(usage_root_dir)
+    # delete_subdire('steps_clean_crop')
+    # delete_subdire('clicked_frames')
+    # delete_subdire('clicked_frames_crop')
     delete_subdire('detected_frames')
     print('all done! :)')
