a9e7438045084307ce3f6ba585a68da0bae974ca
Yixue Zhao <felicitia2010@gmail.com>
2021-08-10 21:15:06 -0400

    refactor for auto batch

--- code/1_step_extraction/special_step_recognition.py
+++ code/1_step_extraction/special_step_recognition.py
@@ -5,113 +5,116 @@ import json, os
 
 ### input parameters you need to change ###
 usage_root_dir = os.path.abspath("/Users/yixue/Documents/Research/UsageTesting/v2s_data/Combined/11-Help") # directory to v2s data of a particular usage
-dir_to_rename = "steps_clean" # will rename the files in this folder to add special cases, such as swipe direction
 ### end of input parameters ###
 
 
-for folder in os.listdir(usage_root_dir):
-    if os.path.isdir(os.path.join(usage_root_dir, folder)):
-        dir_to_json = os.path.join(usage_root_dir, folder, "detected_actions.json")
-        dir_to_steps_clean = os.path.join(usage_root_dir, folder, dir_to_rename)
+def add_special_action(usage_root_dir):
+    dir_to_rename = "steps_clean"  # will rename the files in this folder to add special cases, such as swipe direction
+    for folder in os.listdir(usage_root_dir):
+        if os.path.isdir(os.path.join(usage_root_dir, folder)):
+            dir_to_json = os.path.join(usage_root_dir, folder, "detected_actions.json")
+            dir_to_steps_clean = os.path.join(usage_root_dir, folder, dir_to_rename)
 
-        print("\nStarting "+folder)
-        file = open(dir_to_json,)
-        data = json.load(file)
+            print("\nStarting " + folder)
+            file = open(dir_to_json, )
+            data = json.load(file)
 
-        for j in range(len(data)):
-            act_type = data[j]['act_type']
-            if act_type == "LONG_CLICK":
-                frames = data[j]['frames']
+            for j in range(len(data)):
+                act_type = data[j]['act_type']
+                if act_type == "LONG_CLICK":
+                    frames = data[j]['frames']
 
-                for i in frames:
-                    if len(str(i)) == 1:
-                        img = "bbox-000"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-long.jpg"))
-                            print("renamed "+img+" to long")
-                        except:
-                            pass
-                    elif len(str(i)) == 2:
-                        img = "bbox-00"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-long.jpg"))
-                            print("renamed "+img+" to long")
-                        except:
-                            pass
-                    elif len(str(i)) == 3:
-                        img = "bbox-0"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-long.jpg"))
-                            print("renamed "+img+" to long")
-                        except:
-                            pass
-                    elif len(str(i)) == 4:
-                        img = "bbox-"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-long.jpg"))
-                            print("renamed "+img+" to long")
-                        except:
-                            pass
+                    for i in frames:
+                        if len(str(i)) == 1:
+                            img = "bbox-000" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-long.jpg"))
+                                print("renamed " + img + " to long")
+                            except:
+                                pass
+                        elif len(str(i)) == 2:
+                            img = "bbox-00" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-long.jpg"))
+                                print("renamed " + img + " to long")
+                            except:
+                                pass
+                        elif len(str(i)) == 3:
+                            img = "bbox-0" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-long.jpg"))
+                                print("renamed " + img + " to long")
+                            except:
+                                pass
+                        elif len(str(i)) == 4:
+                            img = "bbox-" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-long.jpg"))
+                                print("renamed " + img + " to long")
+                            except:
+                                pass
 
-                        
-            elif act_type == "SWIPE":
-                # print(data[j]['frames'])
-                frames = data[j]['frames']
-                swipe = ""
 
-                x_1 = data[j]["taps"][0]['x']
-                y_1 = data[j]["taps"][0]['y']
-                
-                x_2 = data[j]["taps"][len(frames)-1]['x']
-                y_2 = data[j]["taps"][len(frames)-1]['y']
+                elif act_type == "SWIPE":
+                    # print(data[j]['frames'])
+                    frames = data[j]['frames']
+                    swipe = ""
 
-                if abs(x_1-x_2) > abs(y_1-y_2):
-                    if x_2>x_1:
-                        swipe = "right"
+                    x_1 = data[j]["taps"][0]['x']
+                    y_1 = data[j]["taps"][0]['y']
+
+                    x_2 = data[j]["taps"][len(frames) - 1]['x']
+                    y_2 = data[j]["taps"][len(frames) - 1]['y']
+
+                    if abs(x_1 - x_2) > abs(y_1 - y_2):
+                        if x_2 > x_1:
+                            swipe = "right"
+                        else:
+                            swipe = "left"
                     else:
-                        swipe = "left"
-                else:
-                    if y_2>y_1:
-                        swipe = "down"
-                    else:
-                        swipe = "up"
-                # print(swipe)
-                for i in frames:
-                    if len(str(i)) == 1:
-                        img = "bbox-000"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
-                            print("renamed "+img+" to "+swipe)
-                        except:
-                            pass
-                    elif len(str(i)) == 2:
-                        img = "bbox-00"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
-                            print("renamed "+img+" to "+swipe)
-                        except:
-                            pass
-                    elif len(str(i)) == 3:
-                        img = "bbox-0"+str(i)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
-                            print("renamed "+img+" to "+swipe)
-                        except:
-                            pass
-                    elif len(str(i)) == 4:
-                        img = "bbox-"+str(i)
-                        print(img)
-                        try:
-                            os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'), os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
-                            print("renamed "+img+" to "+swipe)
-                        except:
-                            pass
-                    
+                        if y_2 > y_1:
+                            swipe = "down"
+                        else:
+                            swipe = "up"
+                    # print(swipe)
+                    for i in frames:
+                        if len(str(i)) == 1:
+                            img = "bbox-000" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
+                                print("renamed " + img + " to " + swipe)
+                            except:
+                                pass
+                        elif len(str(i)) == 2:
+                            img = "bbox-00" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
+                                print("renamed " + img + " to " + swipe)
+                            except:
+                                pass
+                        elif len(str(i)) == 3:
+                            img = "bbox-0" + str(i)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
+                                print("renamed " + img + " to " + swipe)
+                            except:
+                                pass
+                        elif len(str(i)) == 4:
+                            img = "bbox-" + str(i)
+                            print(img)
+                            try:
+                                os.rename(os.path.join(dir_to_steps_clean, img + '.jpg'),
+                                          os.path.join(dir_to_steps_clean, img + "-swipe-" + swipe + ".jpg"))
+                                print("renamed " + img + " to " + swipe)
+                            except:
+                                pass
 
-                    
-                        
-
-        print("Finished "+folder)
-        file.close()
-
-print("\n\nDone")
+            print("Finished " + folder)
+            file.close()
--- code/2_ir_classification/screen_widget_extraction.py
+++ code/2_ir_classification/screen_widget_extraction.py
@@ -12,15 +12,16 @@ import numpy as np
 import math
 
 ### input parameters you need to change ###
-usage_root_dir = os.path.abspath('/Users/yixue/Documents/Research/UsageTesting/v2s_data/Combined/10-Contact')
-uied_result_root_dir = '/Users/yixue/Documents/Research/UsageTesting/Develop/UIED2.3-output-optimized/10-Contact'
-input_dir = 'steps_clean'
-output_dir = 'ir_data_auto'
-v2s_height = 1920 # screen max height in v2s
-uied_height = 800 # image max height in UIED
+# usage_root_dir = os.path.abspath('/Users/yixue/Documents/Research/UsageTesting/v2s_data/Combined/10-Contact')
+# uied_result_root_dir = '/Users/yixue/Documents/Research/UsageTesting/Develop/UIED2.3-output-optimized/10-Contact'
+# input_dir = 'steps_clean'
+# output_dir = 'ir_data_auto'
+# v2s_height = 1920 # screen max height in v2s
+# uied_height = 800 # image max height in UIED
 ### end of input parameters
 
 def extract_screen(step_image_file, app_root_dir):
+    output_dir = 'ir_data_auto'
     filename = os.path.basename(step_image_file).replace('.jpg', '')
     if 'swipe' in filename:
         return # don't copy the screen associated with swipe action
@@ -36,7 +37,10 @@ def extract_screen(step_image_file, app_root_dir):
     # print(src_file, dst_file)
     shutil.copy(src_file, dst_file)
 
-def extract_widget(step_image_file, app_root_dir, detected_actions_json):
+def extract_widget(step_image_file, app_root_dir, detected_actions_json, usage_root_dir):
+    output_dir = 'ir_data_auto'
+    v2s_height = 1920  # screen max height in v2s
+    uied_height = 800  # image max height in UIED
     # if not 'bbox-0285' in os.path.basename(step_image_file):
     #     return
     # make sure to name the file with app_root_folder name in the beginning
@@ -51,9 +55,9 @@ def extract_widget(step_image_file, app_root_dir, detected_actions_json):
     # print('frame id', frame_id_with_touch)
     x_v2s, y_v2s = get_coordinates(detected_actions_json, frame_id_with_touch)
     x_uied, y_uied = normalize_coordinates(x_v2s, y_v2s, v2s_height, uied_height)
-    compo = find_cropped_widget(x_uied, y_uied, app_root_dir, filename)
+    compo = find_cropped_widget(x_uied, y_uied, app_root_dir, filename, usage_root_dir)
     if compo is None:
-        compo_relaxed = find_cropped_widget_relaxed(x_uied, y_uied, app_root_dir, filename, 10) # relax the inside criteria once
+        compo_relaxed = find_cropped_widget_relaxed(x_uied, y_uied, app_root_dir, filename, 10, usage_root_dir) # relax the inside criteria once
         if compo_relaxed is None:
             print('relaxed compo is still None..., frame is', filename)
             print('touch is', x_uied, y_uied)
@@ -87,7 +91,9 @@ def rm_outer_compo(compo_found):
             inner_compos.append(compo_found[i])
     return inner_compos
 
-def find_cropped_widget_relaxed(x, y, app_root_dir, filename, relax_threshold):
+def find_cropped_widget_relaxed(x, y, app_root_dir, filename, relax_threshold, usage_root_dir):
+    uied_result_root_dir = '/Users/yixue/Documents/Research/UsageTesting/Develop/UIED2.3-output-optimized/' \
+                           + os.path.basename(os.path.normpath(usage_root_dir))
     app_root_name = os.path.basename(os.path.normpath(app_root_dir))
     screen_UIEDresult_dir = os.path.join(uied_result_root_dir, app_root_name, app_root_name + '-' + filename + '-screen')
     compo_found = []
@@ -118,7 +124,9 @@ def find_cropped_widget_relaxed(x, y, app_root_dir, filename, relax_threshold):
                 closest_compo = compo
         return closest_compo
 
-def find_cropped_widget(x, y, app_root_dir, filename):
+def find_cropped_widget(x, y, app_root_dir, filename, usage_root_dir):
+    uied_result_root_dir = '/Users/yixue/Documents/Research/UsageTesting/Develop/UIED2.3-output-optimized/' \
+                           + os.path.basename(os.path.normpath(usage_root_dir))
     app_root_name = os.path.basename(os.path.normpath(app_root_dir))
     screen_UIEDresult_dir = os.path.join(uied_result_root_dir, app_root_name, app_root_name + '-' + filename + '-screen')
     compo_found = []
@@ -163,7 +171,9 @@ def normalize_coordinates(x_v2s, y_v2s, v2s_height, uied_height):
     return x_uied, y_uied
 
 
-def main():
+def screen_extraction_auto(usage_root_dir):
+    input_dir = 'steps_clean'
+    output_dir = 'ir_data_auto'
     for step_dir in glob.glob(usage_root_dir + '/*/' + input_dir):
         if not os.path.exists(step_dir.replace(input_dir, output_dir)):
             os.mkdir(step_dir.replace(input_dir, output_dir))
@@ -174,9 +184,37 @@ def main():
             for step_image_file in os.scandir(step_dir):
                 # print(step_image_file.path) # full abs path
                 if step_image_file.path.endswith('.jpg'):
-                    # extract_screen(step_image_file, app_root_dir)
-                    extract_widget(step_image_file, app_root_dir, detected_actions_json)
+                    extract_screen(step_image_file, app_root_dir)
 
-if __name__ == '__main__':
-    main()
-    print('all done! :)')
+def widget_extraction_auto(usage_root_dir):
+    input_dir = 'steps_clean'
+    output_dir = 'ir_data_auto'
+    for step_dir in glob.glob(usage_root_dir + '/*/' + input_dir):
+        if not os.path.exists(step_dir.replace(input_dir, output_dir)):
+            os.mkdir(step_dir.replace(input_dir, output_dir))
+        app_root_dir = step_dir.replace(input_dir, '') # /Users/yixue/Documents/Research/UsageTesting/UsageTesting-Repo/video_data_examples/6pm-video-signin-3/
+        print(app_root_dir)
+        with open(os.path.join(app_root_dir, 'detected_actions.json'), 'r') as f:
+            detected_actions_json = json.load(f)
+            for step_image_file in os.scandir(step_dir):
+                # print(step_image_file.path) # full abs path
+                if step_image_file.path.endswith('.jpg'):
+                    extract_widget(step_image_file, app_root_dir, detected_actions_json, usage_root_dir)
+
+# def main():
+#     for step_dir in glob.glob(usage_root_dir + '/*/' + input_dir):
+#         if not os.path.exists(step_dir.replace(input_dir, output_dir)):
+#             os.mkdir(step_dir.replace(input_dir, output_dir))
+#         app_root_dir = step_dir.replace(input_dir, '') # /Users/yixue/Documents/Research/UsageTesting/UsageTesting-Repo/video_data_examples/6pm-video-signin-3/
+#         print(app_root_dir)
+#         with open(os.path.join(app_root_dir, 'detected_actions.json'), 'r') as f:
+#             detected_actions_json = json.load(f)
+#             for step_image_file in os.scandir(step_dir):
+#                 # print(step_image_file.path) # full abs path
+#                 if step_image_file.path.endswith('.jpg'):
+#                     # extract_screen(step_image_file, app_root_dir)
+#                     extract_widget(step_image_file, app_root_dir, detected_actions_json)
+#
+# if __name__ == '__main__':
+#     main()
+#     print('all done! :)')
