c4c0c72456df99da4db48286fe47ef8c1e3c3556
Yixue Zhao <felicitia2010@gmail.com>
2022-02-28 18:46:46 -0500

    add coverage and recall eval results

--- code/global_config.py
+++ code/global_config.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 FINAL_ARTIFACT_ROOT_DIR = '/Users/yixue/Documents/Research/UsageTesting/Final-Artifacts'
@@ -6,6 +6,9 @@ FINAL_ARTIFACT_ROOT_DIR = '/Users/yixue/Documents/Research/UsageTesting/Final-Ar
 USAGE_REPO_ROOT_DIR = '/Users/yixue/Documents/Research/UsageTesting/UsageTesting-Repo'
 
 PATH_TO_WORD2VEC = '/Users/yixue/Documents/Research/FrUITeR/Develop/CraftDroid/code-release/GoogleNews-vectors-negative300.bin'
+
+EVAL_RESULT_PATH = os.path.join(USAGE_REPO_ROOT_DIR, 'code', 'evaluation', 'all_eval_results.csv')
+
 
 usage_folder_map = {}
 usage_folder_map['signin'] = '1-SignIn'
