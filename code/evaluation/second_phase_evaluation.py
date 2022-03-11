94d20a0152d3068723e8d05310a51159dc1ca309
Saghar Talebipour <saghar.talebipoor@gmail.com>
2022-03-11 07:41:11 -0800

    Added abc model.

--- code/evaluation/second_phase_evaluation.py
+++ code/evaluation/second_phase_evaluation.py
@@ -47,7 +47,7 @@ def evaluation_with_classifiers(AUT, usage_name):
 
 
 if __name__ == '__main__':
-    AUT = Etsy()
-    usage_name = usage_folder_map['signin'] # this usage_folder_map is defined in global_config.py
+    AUT = Abc()
+    usage_name = usage_folder_map['menu'] # this usage_folder_map is defined in global_config.py
     evaluation_with_classifiers(AUT, usage_name)
     print('all done! :)')
